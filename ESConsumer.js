67d2c7d4c55dff8392946664512b62476f002175
chriscouch <couchc@gmail.com>
2016-06-26 13:31:18 -0500

    Updated comsumer. Working version.

--- ESConsumer.js
+++ ESConsumer.js
@@ -1,28 +1,43 @@
 /**
  * Created by Chris on 6/25/16.
  */
+    //This name will be used for the Kafka Topic as well as the ES Index.
+var consumerTopic = 'DNSRecord'
+var indexName = 'dnsrecord'
+
+//Create and connect to ES
+var elasticsearch = require('elasticsearch')
 var esclient = new elasticsearch.Client({
     host: 'localhost:9200',
     log: 'trace'
-});
+})
 
+
+//Create the kafka consumer and and connect to Kafka
 var kafka = require('kafka-node'),
     Consumer = kafka.Consumer,
     client = new kafka.Client(),
     consumer = new Consumer(
         client,
         [
-            { topic: 'DNSRecord', partition: 0 }
+            { topic: consumerTopic, partition: 0 }
         ],
         {
             autoCommit: true
         }
-    );
-consumer.on('message', function (message) {
+    )
 
-  //  console.log(message);
-    //TODO: Add Elastic Search Insert
-});
+//Wait for messages on the Topic
+consumer.on('message', function (message){
+    var msgconvert = message.value
+    esclient.index({
+        index: indexName,
+        type: indexName,
+        body: msgconvert
+    }, function (error, response){})
+})
+
+
 
 
 
